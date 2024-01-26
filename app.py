307a5a090ed063d2041e06b68700486c9d7247f0
QaisLintang <77113381+QaisLintang@users.noreply.github.com>
2024-01-26 20:11:57 +0700

    updated app.py and html file

--- app.py
+++ app.py
@@ -1,32 +1,81 @@
 # app.py (Flask program)
 
 from flask import Flask, render_template
-import csv
 import time
+import mysql.connector
+
+db_config = {
+    'host': '180.250.135.11',
+    'user': 'vm-b',
+    'password': 'admin@123',
+    'database': 'moodle',
+}
 
 class peserta:
-    def __init__(self, user_full_name, IP_address, status, session):
-        self.user_full_name = user_full_name
-        self.IP_address = IP_address
+    def __init__(self, username, userId, sid, first_IP, last_IP, status, timecreated, timemodified):
+        self.username = username
+        self.userId = userId
+        self.first_IP = first_IP
+        self.last_IP = last_IP
+        self.sid = sid
+        self.timecreated = timecreated
+        self.timemodified = timemodified
         self.status = status
-        self.session = session
 
 data = {}
 
 app = Flask(__name__)
 
-# Read CSV file and create a dictionary with usernames as keys and corresponding IP addresses as values
-def read_csv(file_path):
+# # Read CSV file and create a dictionary with usernames as keys and corresponding IP addresses as values
+# def read_csv(file_path):
+#     global data
+#     with open(file_path, 'r') as csv_file:
+#         reader = csv.DictReader(csv_file)
+#         for row in reader:
+#             if "Quiz:" in row["Event context"]:
+#                 if "-" != row['User full name']:
+#                     nama_peserta = row['User full name']
+#                     status = check_cheating(data, row['User full name'], row['IP address'])
+#                     data[nama_peserta] = peserta(row['User full name'], row["IP address"], status)
+#     return data
+
+def load_data():
     global data
-    with open(file_path, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            if "Quiz:" in row["Event context"]:
-                if "-" != row['User full name']:
-                    nama_peserta = row['User full name']
-                    status = check_cheating(data, row['User full name'], row['IP address'])
-                    data[nama_peserta] = peserta(row['User full name'], row["IP address"], status)
+
+    conn = mysql.connector.connect(**db_config)
+
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+
+    # Example query
+    cursor.execute(
+        """ 
+        select mdl_user.id, mdl_user.username,mdl_sessions.userid, mdl_sessions.sid,mdl_sessions.firstip, mdl_sessions.lastip, FROM_UNIXTIME(mdl_sessions.timecreated) AS timecreated, FROM_UNIXTIME(mdl_sessions.timemodified) AS timemodified  
+        from mdl_user
+        inner join mdl_sessions on mdl_user.id=mdl_sessions.userid;
+        """)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if data:
+            if row[2] in data:
+                if data[row[2]].sid != row[3]:
+                    data[row[2]].sid = row[3]
+                if data[row[2]].first_IP != row[4]:
+                    data[row[2]].first_IP = row[4]
+                if data[row[2]].last_IP != row[5]:
+                    data[row[2]].last_IP = row[5]
+                if data[row[2]].timecreated != row[6]:
+                    data[row[2]].timecreated = row[6]
+                if data[row[2]].timemodified != row[7]:
+                    data[row[2]].timemodified = row[7]
+            else:
+                data[row[2]] = peserta(row[1], row[2], row[3], row[4], row[5], "good", row[6], row[7])
+        else:
+            data[row[2]] = peserta(row[1], row[2], row[3], row[4], row[5], "good", row[6], row[7])
+    
     return data
+                    
 
 def check_cheating(data, current_name, current_IP):
     if len(data) == 0:
@@ -47,20 +96,10 @@ def check_cheating(data, current_name, current_IP):
 # Route to display usernames and IP addresses
 @app.route('/')
 def show_usernames():
-    # Assuming your CSV file is named 'data.csv' and is in the same directory as this script
-    i = 0
-    while True:
-        if (i % 30) == 0:
-            csv_file_path = 'dummy_data.csv'
-            user_data = read_csv(csv_file_path)
-            return render_template('usernames.html', user_data=user_data)
-        elif (i % 60) == 0:
-            csv_file_path = 'dummy_IP_Change_data.csv'
-            user_data = read_csv(csv_file_path)
-            return render_template('usernames.html', user_data=user_data)
-        else:
-            i += 1
-            # time.sleep(1)
+    user_data = load_data()
+
+    return render_template('usernames.html', user_data=user_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
