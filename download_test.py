69cf92b1c140a571d36f4b8e4adafdb8f2de9f14
QaisLintang <77113381+QaisLintang@users.noreply.github.com>
2024-05-20 23:38:32 +0700

    update file

--- download_test.py
+++ download_test.py
@@ -1,6 +1,6 @@
 import subprocess
 
 url = "https://sandbox.telkomuniversity.ac.id/laclog/lac-eprt-log/Quiz%20Attempts/1%20April%202024/Shift%201/"
-command = f'python -m wget --user=serverlog --password=S3rverl0g! -r -np -nH --cut-dirs=3 -R "index.html*" {url}'
+command = f'wget --user=serverlog --password=S3rverl0g! -r -np -nH --cut-dirs=3 -R "index.html*" {url}'
 
 subprocess.run(command, shell=True)
